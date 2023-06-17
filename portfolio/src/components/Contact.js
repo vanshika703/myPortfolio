0217148cf4048f9d9a04fa27b1683153909cbe6b
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-06-17 15:24:11 +0530

    fix: add responsiveness

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -6,24 +6,24 @@ import instagram from "../img/instagram.png";
 const Contact = () => {
   return (
     <div className="h-screen w-screen bg-black flex flex-col justify-around items-center">
-      <p className="text-slate-300 font-medium text-[2rem]">
+      <p className="text-slate-300 font-medium text-[2rem] w-2/3 sm:w-auto">
         Let's <span className="italic">collaborate</span> to create an
         impressive online presence<br></br> that drives your success.
       </p>
 
-      <div className="flex justify-around items-center w-2/3">
+      <div className="flex justify-around items-center sm:flex-row flex-col">
         <button className="text-slate-300 font-medium text-[1.4rem]">
           Schedule a call today
         </button>
-          <p className="text-slate-300 text-[1.4rem]">
-            vanshikabhojnagarwala2000@gmail.com
-          </p>
+        <p className="text-slate-300 text-[1.4rem] px-10">
+          vanshikabhojnagarwala2000@gmail.com
+        </p>
 
-          <div className="flex justify-center">
-            <img src={twitter} alt="social" className="w-6 h-6 m-2" />
-            <img src={linkedin} alt="social" className="w-6  h-6 m-2" />
-            <img src={instagram} alt="social" className="w-6 h-6  m-2" />
-          </div>
+        <div className="flex justify-center">
+          <img src={twitter} alt="social" className="w-6 h-6 m-2" />
+          <img src={linkedin} alt="social" className="w-6  h-6 m-2" />
+          <img src={instagram} alt="social" className="w-6 h-6  m-2" />
+        </div>
       </div>
     </div>
   );
--- portfolio/src/components/Samples.js
+++ portfolio/src/components/Samples.js
@@ -1,19 +1,27 @@
 import React from "react";
-import youtube from "../img/work1.png";
+import work1 from "../img/work1.png";
+import work2 from "../img/work2.png";
+import work3 from "../img/work3.png";
+import work4 from "../img/work4.png";
+import work5 from "../img/work5.png";
+import work6 from "../img/work6.png";
 
 const Samples = () => {
   return (
-    <div className="h-screen w-screen bg-black text-center">
+    <div className="w-screen bg-black text-center">
       <p className="text-slate-300 font-medium text-[2rem]">
-        Don't believe me?
+        don't <span className="italic">believe</span> me?
       </p>
-      <div className="flex flex-wrap justify-center items-center">
-        <img src={youtube} alt="projects" className="w-96 h-52 rounded m-5" />
-        <img src={youtube} alt="projects" className="w-96 h-52 rounded m-5" />
-        <img src={youtube} alt="projects" className="w-96 h-52 rounded m-5" />
-        <img src={youtube} alt="projects" className="w-96 h-52 rounded m-5" />
-        <img src={youtube} alt="projects" className="w-96 h-52 rounded m-5" />
-        <img src={youtube} alt="projects" className="w-96 h-52 rounded m-5" />
+      <p className="text-slate-400 font-medium text-[1.4rem]">have a look 👀</p>
+      <div className="flex flex-wrap justify-center items-center mt-10">
+        <img src={work6} alt="projects" className="w-96 h-48 rounded m-5" />
+        <img src={work1} alt="projects" className="w-96 h-48 rounded m-5" />
+        <img src={work3} alt="projects" className="w-96 h-48 rounded m-5" />
+
+        <img src={work2} alt="projects" className="w-96 h-48 rounded m-5" />
+        <img src={work5} alt="projects" className="w-96 h-48 rounded m-5" />
+
+        <img src={work4} alt="projects" className="w-96 h-48 rounded m-5" />
       </div>
     </div>
   );
